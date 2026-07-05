a73d36b096a6ede3123d8db9f266d839f5a4be56
hemromhub-source <hemromhub@gmail.com>
2026-07-05 07:09:11 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
+  env: {
+    NEXT_PUBLIC_SITE_NAME: '99 VLE Service',
+  },
+};
+
+module.exports = nextConfig;
